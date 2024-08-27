33152c0fe9035d87b94a100678b8ce38293ffd22
LOYGAN <85824464+LoanNINI@users.noreply.github.com>
2024-08-27 21:03:48 +0700

    Update

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.Hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+function NextDownPage(){
+    window.scrollTo(0,850)
+}
+
+function Information_Roblox(){
+    var element = document.querySelector("#SKILLNFO");
+    element.classList.replace("Skill-Info", "Skill-Info_Roblox");
+}
